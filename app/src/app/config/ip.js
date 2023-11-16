70f3dd3bb71d08954e0a9da3c6da00981fe15fb6
Ifechukwudeni Oweh <teddyoweh@gmail.com>
2023-11-16 03:44:51 -0600

    Update ip.js

--- app/src/app/config/ip.js
+++ app/src/app/config/ip.js
@@ -1,5 +1,8 @@
 // export const ip = "http://localhost:3030"
 // export const socketip = "ws://localhost:3050"
 
-export const ip = "https://snapbrain-28f92826bdb8.herokuapp.com"
-export const socketip = "wss://snapbrain-28f92826bdb8.herokuapp.com"
+// export const ip = "https://snapbrain-28f92826bdb8.herokuapp.com"
+// export const socketip = 'ws://snapbrain-28f92826bdb8.herokuapp.com';
+
+export const ip = "http://50.116.24.45:3030"
+export const socketip = "ws://50.116.24.45:3050"
